14bbaebf78fe7032c80177274a9d3448da0c17a0
Zachary Shold <sholdzb@gmail.com>
2021-03-23 12:52:32 -0500

    averageChars now properly converts input data into return object

--- reviews/methods/averageChars.js
+++ reviews/methods/averageChars.js
@@ -1,22 +1,50 @@
 const averageChars = (charArr) => {
+  debugger;
   let result = {};
   let counts = {};
+  review_ids = [];
+  result.recommend = {true: 0, false: 0};
+  result.rating = 0;
+  counts.rating = 0;
+
 
   charArr.forEach(obj => {
-    if (!result[obj.name]) {
-      result[obj.name] = {id : obj.id,
-                          value : Number(obj.value)};
-      counts[obj.name] = 1;
+    if (!review_ids.includes(obj.review_id)) {
+      review_ids.push(obj.review_id);
+      if (!result[obj.name]) {
+        result[obj.name] = {id : obj.id,
+                            value : Number(obj.value)};
+        counts[obj.name] = 1;
+      } else {
+        result[obj.name].value += Number(obj.value);
+        counts[obj.name]++;
+      }
+      if (obj.recommend) {
+        result.recommend.true++;
+      } else {
+        result.recommend.false++;
+      }
+      result.rating += obj.rating;
+      counts.rating += 1
     } else {
-      result[obj.name].value += Number(obj.value);
-      counts[obj.name]++;
+      if (!result[obj.name]) {
+        result[obj.name] = {id : obj.id,
+                          value : Number(obj.value)};
+        counts[obj.name] = 1;
+      } else {
+        result[obj.name].value += Number(obj.value);
+        counts[obj.name]++;
+      }
     }
-  });
+  })
+
   for (key in result) {
-    result[key] = result[key].value / counts[key];
+    if (typeof result[key] !== 'object') {
+      result[key] = result[key].value / counts[key];
+    }
   }
   return result;
-}
+  };
 
 module.exports = {
   averageChars,
